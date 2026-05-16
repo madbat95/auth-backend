8062e3ada3909442b864a98b96c863ef197e3136
madbat95 <hamahmed95@gmail.com>
2026-05-17 01:18:11 +0500

    added users module adn first ever api

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, ConflictException } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('register')
+  register(@Body() dto: RegisterDto) {
+    const exists = this.usersService.findByEmail(dto.email);
+    if (exists) {
+      throw new ConflictException('Email already exists');
+    }
+
+    const user = this.usersService.create(dto.email, dto.password);
+    const { password, ...result } = user;
+    return result;
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './user.interface';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+
+    private users: User[] = [];
+    private nextId: number = 1;
+
+
+    findByEmail(email:string): User | undefined {
+        return this.users.find(u => u.email === email);
+    }
+
+    create(email: string, password:string): User{
+        const user: User = {
+            id: this.nextId++,
+            email, password
+        }
+        this.users.push(user);
+        return user;
+
+    }
+
+
+
+
+
+
+
+
+
+}
