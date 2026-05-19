e4b8c7a33da93faea6a3fdc0aefe1fff0fc3d9a9
madbat95 <hamahmed95@gmail.com>
2026-05-19 17:28:06 +0500

    added jwt token creation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(email: string, password: string) {
+    const user = this.usersService.findByEmail(email);
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) {
+      throw new UnauthorizedException('invalid credentials');
+    }
+
+    const payload = { sub: user.id, email: user.email };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './user.interface';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -10,13 +11,24 @@ export class UsersService {
     return this.users.find((u) => u.email === email);
   }
 
-  create(email: string, password: string): User {
+  async create(email: string, password: string): Promise<User> {
+    const hashedPassword = await bcrypt.hash(password, 10);
     const user: User = {
       id: this.nextId++,
-      email,
-      password,
+      email: email,
+      password: hashedPassword,
     };
     this.users.push(user);
     return user;
   }
+
+  //   create(email: string, password: string): User {
+  //     const user: User = {
+  //       id: this.nextId++,
+  //       email,
+  //       password,
+  //     };
+  //     this.users.push(user);
+  //     return user;
+  //   }
 }
